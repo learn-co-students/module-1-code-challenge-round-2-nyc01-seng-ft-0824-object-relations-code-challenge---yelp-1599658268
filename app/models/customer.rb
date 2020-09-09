fc2deb25761991937a93769cd5fe89b4f13bb064
brynknowles <bryn.knowles@gmail.com>
2020-09-09 11:38:00 -0400

    final commmit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -34,9 +34,30 @@ class Customer
   end
 
   def self.find_by_name(name)
-    if self.name == name
-      name.first
+            ## This method? (doesn't work)
+    if self.full_name == name # why can't I use my full_name method here?
+      self
     end
+            ## OR this method (doesn't work)
+    if self == name  #This one won't work either. not sure what I'm doing wrong.
+      self.full_name
+    end
+
+    ## I know that I need to go the list of full names and match the argument that is passed in, 
+    ## and then return the first person. 
+    ## I would use a .first here once I found all the names that match.
+  end
+
+  def self.find_all_by_given_name(name)
+    all_names = []
+    if self.given_name == name # again, I'm missing something here and not sure what it is.
+      all_names << name
+    end
+    all_names
+
+    ##  Again, I know I need to go through all of the given names that match the argument of name that is passed in,
+    ##  and then collect it in an array
+    ##  Once I have completed the iteration, I need to return that array which will provide all the customers with that given name
   end
 
 end
