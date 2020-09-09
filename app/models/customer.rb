7b39dae47031f09568f01251a35ccdab948b2b33
ashabahmed <ashabahmed7@gmail.com>
2020-09-09 09:50:37 -0400

    first set of deliverables completed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,19 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,11 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+john = Customer.new("John", "Joe")
+goodeats = Restaurant.new("GoodEats")
+
+review1 = Review.new(john, goodeats, 5)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
